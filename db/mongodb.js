af5d670d43c3f55bce340324bd4af070a6449671
Joscript <39909905+Joscriptt@users.noreply.github.com>
2023-08-23 16:52:01 +0100

    Update mongodb.js

--- db/mongodb.js
+++ db/mongodb.js
@@ -10,7 +10,7 @@ export const connectDB= async()=>{
     }
 
     // mongodb://127.0.0.1:27017/
-    // "mongodb+srv://josephhjamess46:Lln46W2aiMFUYPQ9@cluster0.zfyquyg.mongodb.net/test"
+    
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/test", {
         // await mongoose.connect(process.env.MONGO_URI, {
